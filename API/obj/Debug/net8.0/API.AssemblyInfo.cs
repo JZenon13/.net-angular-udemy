003f2c431ec7260880c3ef4f77486aae47a2c900
Jesse Zenon <jz.the.dev@gmail.com>
2024-07-02 16:18:57 -0500

    7/2

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d258040e956362a28b89c911cc5d210234e5a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d420642e950b2b8304b260a33aefe57a2de3851")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
