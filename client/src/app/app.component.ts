ab235f27da488b4ce2500075490c1eb5fe2095a6
Jesse Zenon <jz.the.dev@gmail.com>
2024-07-10 16:15:57 -0500

    7/10

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,20 +2,36 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
+  private accountService = inject(AccountService);
   title = 'App';
   users: any;
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
     this.http.get('http://localhost:5002/api/users').subscribe({
       next: (response) => (this.users = response),
       error: (err) => console.error(err),
